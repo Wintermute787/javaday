e2b6bc11c1f415dd0ac2aedfe7b128c4a6cd96ef
Wintermute787 <joebarnes333@gmail.com>
2021-10-20 22:23:06 -0700

    add loop logic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int day = 9;
-        dayOfWeek(day);
+        int day1 = 9;
+        int day2 = 3;
+        dayOfWeek(day1);
+        dayOfWeek2(day2);
 
     }
     public static void dayOfWeek(int day) {
@@ -17,6 +19,22 @@ public class Main {
             case 5 -> System.out.println("Friday");
             case 6 -> System.out.println("Saturday");
             default -> System.out.println("invalid number");
+        }
+    }
+    public static void dayOfWeek2(int day) {
+        if(day > 7 || day < 0){
+            System.out.println("Invalid number");
+        }
+        for(int i = 0; i < 7; i++){
+            switch(i){
+                case 0 -> System.out.println("Sunday");
+                case 1 -> System.out.println("Monday");
+                case 2 -> System.out.println("Tuesday");
+                case 3 -> System.out.println("Wednesday");
+                case 4 -> System.out.println("Thursday");
+                case 5 -> System.out.println("Friday");
+                case 6 -> System.out.println("Saturday");
+            }
         }
     }
 }
