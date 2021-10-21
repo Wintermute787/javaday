dad47a41ea31b61c135089f8859b5b44700d236e
Wintermute787 <joebarnes333@gmail.com>
2021-10-20 22:10:54 -0700

    add more stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("this is a test");
+        int day = 9;
+        dayOfWeek(day);
+
+    }
+    public static void dayOfWeek(int day) {
+        switch (day) {
+            case 0 -> System.out.println("Sunday");
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            default -> System.out.println("invalid number");
+        }
     }
 }
